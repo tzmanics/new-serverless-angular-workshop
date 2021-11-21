53bb7ba4923e51967d73a4dbc09856e9602b5dad
Tara Z. Manicsic <tzmanics@gmail.com>
2021-11-20 21:14:07 -0500

    adds org model

---
+++ src/app/Organization.ts
@@ -0,0 +1,10 @@
+export interface Organization {
+  id: string;
+  name: string;
+  description: string;
+  url: string;
+  donationAmount: string;
+  body: string;
+  website: string;
+  twitter: string;
+}
