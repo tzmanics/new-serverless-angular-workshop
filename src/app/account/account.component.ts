382a1e3d36816b8a6e618fe4d081e4b4694806b5
Tara Z. Manicsic <tzmanics@gmail.com>
2021-11-26 14:43:58 -0500

    adds auth0 login

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
+import { Component, Inject, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
-  styleUrls: ['./account.component.css']
+  styleUrls: ['./account.component.css'],
 })
 export class AccountComponent implements OnInit {
+  profileJson: string = '';
 
-  constructor() { }
+  constructor(
+    public auth: AuthService,
+    @Inject(DOCUMENT) private doc: Document
+  ) {}
 
   ngOnInit(): void {
+    this.auth.user$.subscribe(
+      (profile) => (this.profileJson = JSON.stringify(profile, null, 2))
+    );
   }
 
+  loginWithRedirect() {
+    this.auth.loginWithRedirect();
+  }
+
+  logout() {
+    this.auth.logout({
+      returnTo: this.doc.location.origin,
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AuthModule } from '@auth0/auth0-angular';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
@@ -12,6 +14,10 @@ import { NavigationComponent } from './navigation/navigation.component';
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule,
     HttpClientModule,
+    AuthModule.forRoot({
+      domain: 'tzmanics.auth0.com',
+      clientId: 'kDdgqBa0zN-v_pYu3DtqUfGw8PgEcwxx',
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent],
